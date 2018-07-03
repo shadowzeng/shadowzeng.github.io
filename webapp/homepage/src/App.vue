bc223a415b991adeccf25e519c4a7206eec5e20d
shadowzeng <540714513@qq.com>
2018-07-03 23:05:28 +0800

    create homepage

---
+++ webapp/homepage/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <a class="avatar" href="/">
+      <img src="./assets/avatar.jpg">
+    </a>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 150px;
+}
+#app a img {
+  width: 100px;
+  border-radius: 50%;
+  box-shadow: 1px 2px 8px -2px grey;
+}
+</style>
---
+++ webapp/homepage/src/components/HelloWorld.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <h2>Dear Rebecca</h2>
+    <h3>吴雨桐</h3>
+    <hr>
+    <div class="nav">
+      <ul>
+        <li class="item"><a href="">首页</a></li>
+        <li class="item"><a href="">文章</a></li>
+        <li class="item"><a href="">项目</a></li>
+        <li class="item"><a href="">更多</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+}
+li:after {
+  content: '|';
+  font-size: 12px;
+  color: #ccc;
+  width: 20px;
+  display: inline-block;
+}
+li:last-of-type:after {
+  content: '';
+  width: 0;
+}
+a {
+  font-size: 13px;
+  text-decoration: none;
+  color: #6b6882;
+  transition: all 0.4s;
+}
+hr {
+  max-width: 400px;
+  width: 40%;
+  height: 1px;
+  border: none;
+  background-image: -webkit-linear-gradient(0deg, transparent, #d5d5d5, transparent);
+}
+</style>
