ebeb640cbba4c5e06c2481fb3f1d5bc50469ae09
zeng Shadow <540714513@qq.com>
2018-09-12 19:44:00 +0800

    blog test

--- test/test.js
+++ test/test.js
@@ -1,57 +1,22 @@
-function Animal(){
-    this.species = "动物";
-}
-function Cat(name,color){
-    debugger
-    this.name = name;
-    this.color = color;
-}
-
-Cat.prototype = new Animal();
-var cat1 = new Cat("大毛","黄色");
-debugger
-Cat.prototype.constructor = Cat;
-debugger
-var cat2 = new Cat("大毛","黄色");
-debugger
-/*
-ChildClass = RootClass.extend({
-    constructor: ChildClass,
-    init:function(){}
-});
-
-function enableClassExtend(RootClass){
-    RootClass.$constructor = RootClass;
-    RootClass.extend = function(proto){
-
-        var ExtendedClass = function(){
-            if (!proto.$constructor) {
-                superClass.apply(this, arguments);
-            }
-            else {
-                proto.$constructor.apply(this, arguments);
-            }
-        };
-
-        attextend(ExtendedClass.prototype,proto);
-
-        ExtendedClass.extend = this.extend;
-        ExtendedClass.superCall = superCall;
-        ExtendedClass.superApply = superApply;
-        zrUtil.inherits(ExtendedClass, this);
-        ExtendedClass.superClass = superClass;
-
-        ExtendedClass.prototype = this.prototype;
-        proto
-
+function makeClosures(arr, fn) {
+    var fnArr = [];
+      
+    for (var i = 0; i < arr.length; i++){
+        var e = arr[i];
+        fnArr.push((function(x){
+            return function(){
+                return fn(x);
+            };
+        })(e));
     }
+      
+    return fnArr;
+}
+var tt = function(x){
+    return x*x;
 }
 
-function attextend(target, source) {
-    for (var key in source) {
-        if (source.hasOwnProperty(key)) {
-            target[key] = source[key];
-        }
-    }
-    return target;
-}*/
+var res = makeClosures([1,2,3],tt);
+
+var f1 = res[1]();
+debugger
