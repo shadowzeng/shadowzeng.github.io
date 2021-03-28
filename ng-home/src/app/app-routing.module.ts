dac0525112d6add03591ccdd907bd278dbeeb847
shadowzeng <540714513@qq.com>
2021-03-28 13:05:17 +0800

    chore: welcome page style

--- ng-home/src/app/app-routing.module.ts
+++ ng-home/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
 
 const routes: Routes = [
-  {path: '', redirectTo: 'view', pathMatch: 'full'},
+  {path: '', redirectTo: 'viewer', pathMatch: 'full'},
   {path: 'viewer', loadChildren: () => import('./mindmap-viewer').then(m => m.MindmapViewerModule)},
   {path: 'editor', loadChildren: () => import('./mindmap-editor').then(m => m.MindmapEditorModule)},
 ]
