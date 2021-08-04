977353c8fddc3827005360906821cc24eae8df58
shadowzeng <540714513@qq.com>
2021-08-04 21:24:03 +0800

    chore: use webpack build in angular.json

--- ng-home/webpack.config.js
+++ ng-home/webpack.config.js
@@ -1,8 +1,10 @@
 const ngToolsWebpack = require('@ngtools/webpack')
 const path = require('path')
 
-const workspaceRoot = path.resolve(__dirname, './');
-const projectRoot = path.resolve(__dirname, './');
+const workspaceRoot = path.resolve(__dirname, './')
+const projectRoot = path.resolve(__dirname, './')
+
+// 参考https://github.com/angular/angular-cli/blob/master/packages/angular_devkit/build_webpack/test/angular-app/webpack.config.js
 
 module.exports = {
     mode: 'development',
@@ -15,35 +17,35 @@ module.exports = {
     },
     output: {
         path: path.resolve(workspaceRoot, './dist'),
-        filename: `[name].js`,
+        filename: '[name].js',
     },
     plugins: [
-        new ngToolsWebpack.AngularCompilerPlugin({
+        new ngToolsWebpack.AngularWebpackPlugin({
             tsConfigPath: path.resolve(projectRoot, './tsconfig.app.json')
         })
     ],
     module: {
         rules: [
-            { test: /\.css$/, loader: 'raw-loader' },
-            { test: /\.html$/, loader: 'raw-loader' },
-            { test: /\.ts$/, loader: '@ngtools/webpack' },
+            {test: /\.css$/, loader: 'raw-loader'},
+            {test: /\.html$/, loader: 'raw-loader'},
+            {test: /\.ts$/, loader: '@ngtools/webpack'},
             {
                 test: /\.component\.(css|sass|scss)$/,
                 exclude: [path.resolve('node_modules'), path.resolve('src/styles.scss')],
                 include: [path.resolve('src/app')],
                 use: [
                     'raw-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [
-                                require('postcss-modules')({
-                                    generateScopedName: "[hash:base64:5]"
-                                }),
-                                require('postcss-import')
-                            ]
-                        }
-                    },
+                    // {
+                    //     loader: 'postcss-loader',
+                    //     options: {
+                    //         plugins: [
+                    //             require('postcss-modules')({
+                    //                 generateScopedName: '[hash:base64:5]'
+                    //             }),
+                    //             require('postcss-import')
+                    //         ]
+                    //     }
+                    // },
                     'sass-loader'
                 ]
             },
@@ -52,4 +54,4 @@ module.exports = {
     devServer: {
         historyApiFallback: true
     }
-};
+}
