224471c6f4687bd844a4c12458c7bff2be3c9b04
shadowzeng <540714513@qq.com>
2021-12-17 17:06:21 +0800

    chore: playground

--- playground/_algo/math/excel_column.js
+++ playground/_algo/math/excel_column.js
@@ -1,4 +1,30 @@
 
-function toColumnName() {
-
+/**
+ * A    1   0*26 + 1
+ * B    2   0*26 + 2
+ * ...
+ * Z   26   0*26 + 26
+ * AA  27   1*26 + 1
+ * AB  28   1*26 + 2
+ * ...
+ * AZ  52
+ * BA  53   2*26 + 1
+ *
+ * a0*26^n + a1*26^(n-1) + ... + an*26^0 = num
+ * 1 <= ai <= 26
+ * 这里需要减去1才能看作是26进制，才可以做整除操作
+ * a0*26^n + a1*26^(n-1) + ... + (an - 1) = num - 1
+ *
+ * (num - 1) % 26 值为 an - 1
+ */
+function toColumnName(num) {
+    const arr = []
+    while(num > 0) {
+        const yu = (num - 1) % 26
+        arr.push(yu)
+        num = ((num - 1) - yu) / 26
+    }
+    return arr.reverse().map(n => String.fromCharCode(n + 'A'.charCodeAt(0))).join('')
 }
+
+console.log(toColumnName(53))
