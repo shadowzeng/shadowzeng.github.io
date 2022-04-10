6d5ba3274a15c919abc4d03f073a6f77838ef6cb
shadowzeng <540714513@qq.com>
2022-04-10 11:33:09 +0800

    chore: test yapi

--- playground/react/umi-app/src/pages/index.tsx
+++ playground/react/umi-app/src/pages/index.tsx
@@ -1,6 +1,14 @@
 import styles from './index.less';
+import {request} from 'umi'
+import { useEffect } from 'react';
 
 export default function IndexPage() {
+  useEffect(() => {
+    request("http://yapi.smart-xwork.cn/mock/147168/user/list").then(response => {
+      console.log(response)
+    })
+  });
+
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
